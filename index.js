f44d45a0dd983b735d8e118375d895b3147562f2
Prasadkadam03 <prasadkadam29503@gmail.com>
2024-08-14 14:52:37 +0530

    create route to search user by id

--- index.js
+++ index.js
@@ -31,6 +31,19 @@ app.get('/users', (req, res) => {
     });
 });
 
+app.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const query = `SELECT * FROM users WHERE id = ?`;
+    connection.query(query, [id], (err, result) => {
+        if (err) {
+            console.error('Error retrieving user:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        res.json(result);
+    });
+});
+
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
